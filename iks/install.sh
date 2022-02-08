e5d3b0dd4d2aed1e2e45306c56cb5be6997b1f0d
Chris Ahl <cahl@redhat.com>
2022-02-08 09:22:17 -0500

    update the way iks ibmcloud install is done (#33)
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- iks/install.sh
+++ iks/install.sh
@@ -6,6 +6,15 @@ if [[ "$OS" == "darwin" ]]; then
     if [ -z "$(which jq)" ]; then
         brew install jq
     fi
+    if [ -z "$(which ibmcloud)" ]; then
+      curl -fsSL https://clis.cloud.ibm.com/install/osx | sh
+      if [ $? -ne 0 ]; then
+        echo "IBM Cloud CLI installation failed, exiting with a failure"
+        exit 1;
+      else
+        echo "IBM Cloud CLI version $(ibmcloud version) installed successfullly."
+      fi
+    fi
 elif [[ "$OS" == "linux" ]]; then
     if [ -z "$JENKINS_HOME" ]; then
       # RHEL Linux
@@ -18,30 +27,26 @@ elif [[ "$OS" == "linux" ]]; then
           apk add jq
       fi
     fi
+    if [ -z "$(which ibmcloud)" ]; then
+      curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+      if [ $? -ne 0 ]; then
+        echo "IBM Cloud CLI installation failed, exiting with a failure"
+        exit 1;
+      else
+        echo "IBM Cloud CLI version $(ibmcloud version) installed successfullly."
+      fi
+    fi
 else
     echo "Unsupported OS"
     exit 1
 fi
 
-if [ -z "$(which ibmcloud)" ]; then
-    wget https://clis.cloud.ibm.com/download/bluemix-cli/latest/linux64 -O ibmcloud-cli.tar.gz
-    tar -xvzf ibmcloud-cli.tar.gz
-    rm ibmcloud-cli.tar.gz
-    Bluemix_CLI/install
-    if [ $? -ne 0 ]; then
-      echo "IBM Cloud CLI installation failed, exiting with a failure"
-      exit 1;
-    else
-      echo "IBM Cloud CLI version $(ibmcloud version) installed successfullly."
-    fi
-
-    ibmcloud version
-    curl --progress-bar -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-    ibmcloud plugin install container-service -f
-    if [ $? -ne 0 ]; then
-      echo "IBM Cloud CLI kubernetes plugin installation failed, exiting with a failure"
-      exit 1;
-    else
-      echo "IBM Cloud CLI kubernetes pluing version installed successfullly."
-    fi
+ibmcloud version
+curl --progress-bar -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ibmcloud plugin install container-service -f
+if [ $? -ne 0 ]; then
+  echo "IBM Cloud CLI kubernetes plugin installation failed, exiting with a failure"
+  exit 1;
+else
+  echo "IBM Cloud CLI kubernetes pluing version installed successfullly."
 fi
